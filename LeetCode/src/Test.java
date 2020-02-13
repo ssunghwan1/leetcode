37826ab11b454d6eeb2008c8632d431a534e638e
최성환 <@DESKTOP-VC7O8IJ>
2020-02-13 21:05:30 +0900

    Start!

---
+++ LeetCode/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Solution sol = new Solution();
+		int[] nums = new int[100];
+		int target =0;
+		System.out.println(sol.twoSum(nums, target));
+	}
+
+}
